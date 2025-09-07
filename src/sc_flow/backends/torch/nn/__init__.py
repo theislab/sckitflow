17571c455c636e0b2fd1bbadf4e79cd5feca9b48
FMGS666 <b00794799@essec.edu>
2025-09-07 17:37:42 +0200

    Removing import statements from undefined modules.

--- src/sc_flow/backends/torch/nn/__init__.py
+++ src/sc_flow/backends/torch/nn/__init__.py
@@ -2,14 +2,8 @@ from sc_flow.backends.torch.nn._modules import (
     MLP,
     Resnet1d,
 )
-from sc_flow.backends.torch.nn._vf import (
-    BaseVelocityField,
-    VanillaMLPVelocityField,
-)
 
 __all__ = [
     "MLP",
     "Resnet1d",
-    "BaseVelocityField",
-    "VanillaMLPVelocityField",
 ]

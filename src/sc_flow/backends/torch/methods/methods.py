818e0df83773e05a481b0a4661542742cc334653
Dominik Klein <dominik.klein@helmholtz-munich.de>
2025-10-07 12:24:38 +0200

    first draft OTFlowMatching in jax

---
+++ src/sc_flow/backends/torch/methods/methods.py
@@ -0,0 +1,15 @@
+import sc_flow.methods as basemethods
+
+__all__ = ["FlowMatching", "OTFlowMatching", "GENOT"]
+
+
+class FlowMatching(basemethods.FlowMatching):
+    pass
+
+
+class OTFlowMatching(basemethods.OTFlowMatching):
+    pass
+
+
+class GENOT(basemethods.GENOT):
+    pass

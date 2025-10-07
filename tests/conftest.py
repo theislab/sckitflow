326017c2313bf1000e26e1100fdebfb7f94c32b5
Mariia Minaeva <minaeva.mav@gmail.com>
2025-10-07 16:43:29 +0200

    refactored tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
 import anndata as ad
 import numpy as np
 import pytest
+from .test_utils import get_dummy_network
+import torch
+
+input_dim = 10
+output_dim = 10
+hidden_dims = (20, 20)
+batch_size = 32
 
 
 @pytest.fixture
@@ -9,3 +16,49 @@ def adata():
     adata.layers["scaled"] = np.array([[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]]).astype(np.float32)
 
     return adata
+
+
+@pytest.fixture
+def dummy_method(input_di=input_dim, output_dim=output_dim, hidden_dims=hidden_dims):
+    return get_dummy_network(input_dim, output_dim, hidden_dims)
+
+
+@pytest.fixture
+def dummy_callbacks():
+    class DummyCallbacks:
+        def __init__(self):
+            self.called_with = []
+
+        def run_on_valid_step(self, validation_dict, condition):
+            self.called_with.append((validation_dict, condition))
+            return {"val_loss": 0.456}
+
+    return DummyCallbacks()
+
+
+@pytest.fixture
+def dummy_trainloader():
+    class DummyTrainLoader:
+        def __init__(self):
+            self.sample_calls = 0
+
+        def sample(self, _):
+            self.sample_calls += 1
+            return {"source": torch.rand((batch_size, output_dim)), 
+                    "target": torch.rand((batch_size, output_dim))}
+
+    return DummyTrainLoader()
+
+@pytest.fixture
+def dummy_valloader():
+    class DummyValLoader:
+        def __init__(self):
+            self.sample_calls = 0
+
+        def sample(self, _):
+            self.sample_calls += 1
+            return {"condA": {"source": torch.rand((batch_size, output_dim)), 
+                    "target": torch.rand((batch_size, output_dim))}}
+
+    return DummyValLoader()
+

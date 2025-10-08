fc951295db7a95e22d2483a0783910aecb9e2b8d
lorenzo-consoli <lorenzo.consoli@proton.me>
2025-10-08 15:02:49 +0200

    updating dummy adata and wrote the conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,147 @@
-import anndata as ad
-import numpy as np
+from collections.abc import Sequence
+
 import pytest
+
+from sc_flow.data.dummy import get_dummy_adata
 
 
 @pytest.fixture
-def adata():
-    adata = ad.AnnData(X=np.array([[1.2, 2.3], [3.4, 4.5], [5.6, 6.7]]).astype(np.float32))
-    adata.layers["scaled"] = np.array([[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]]).astype(np.float32)
+def n_obs_pert() -> int:
+    return 50_000
 
-    return adata
+
+@pytest.fixture
+def n_obs_ctrl() -> int:
+    return 10_000
+
+
+@pytest.fixture
+def n_genes() -> int:
+    return 400
+
+
+@pytest.fixture
+def nunique_drugs() -> int:
+    return 3
+
+
+@pytest.fixture
+def nunique_kos() -> int:
+    return 5
+
+
+@pytest.fixture
+def nunique_targets() -> int:
+    return 5
+
+
+@pytest.fixture
+def nunique_source_splits() -> int:
+    return 5
+
+
+@pytest.fixture
+def paired_condition_n_feats() -> int:
+    return 100
+
+
+@pytest.fixture
+def drug_rep_n_feats() -> int:
+    return 512
+
+
+@pytest.fixture
+def ko_rep_n_feats() -> int:
+    return 512
+
+
+@pytest.fixture
+def uns_keys() -> Sequence[str]:
+    return ("drug", "ko")
+
+
+@pytest.fixture
+def is_control_val() -> str:
+    return "control"
+
+
+@pytest.fixture
+def obs_columns_to_nunique_and_prefix(
+    nunique_drugs: int,
+    nunique_kos: int,
+    nunique_targets: int,
+    nunique_source_splits: int,
+) -> dict[str, tuple[int | str]]:
+    return {
+        "drugA": (nunique_drugs, "drug"),
+        "drugB": (nunique_drugs, "drug"),
+        "koA": (nunique_kos, "ko"),
+        "koB": (nunique_kos, "ko"),
+        "target": (nunique_targets, "target"),
+        "source_split": (nunique_source_splits, "source_split"),
+    }
+
+
+@pytest.fixture
+def obs_columns_to_fixed_val(
+    is_control_val: str,
+) -> dict[str, str]:
+    return {
+        "drugA": is_control_val,
+        "drugB": is_control_val,
+        "koA": is_control_val,
+        "koB": is_control_val,
+    }
+
+
+@pytest.fixture
+def obsm_keys_to_dim(
+    paired_condition_n_feats: int,
+) -> dict[str, int]:
+    return {
+        "drugA_time": 1,
+        "drugA_dose": 1,
+        "drugB_time": 1,
+        "drugB_dose": 1,
+        "koA_time": 1,
+        "koA_dose": 1,
+        "koB_time": 1,
+        "koB_dose": 1,
+        "paired_condition": paired_condition_n_feats,
+    }
+
+
+@pytest.fixture
+def uns_keys_to_nunique_prefix_and_dim(
+    nunique_drugs: int,
+    nunique_kos: int,
+    drug_rep_n_feats: int,
+    ko_rep_n_feats: int,
+) -> dict[str, int | str]:
+    return {
+        "drug": (nunique_drugs, "drug", drug_rep_n_feats),
+        "ko": (nunique_kos, "ko", ko_rep_n_feats),
+    }
+
+
+@pytest.fixture
+def adata(
+    n_obs_pert: int,
+    n_obs_ctrl: int,
+    n_genes: int,
+    obsm_keys_to_dim: dict[str, int],
+    obs_columns_to_nunique_and_prefix: dict[str, tuple[int | str]],
+    uns_keys_to_nunique_prefix_and_dim: dict[str, int | str],
+    obs_columns_to_fixed_val: dict[str, str],
+    control_key: str,
+):
+    return get_dummy_adata(
+        n_obs_pert=n_obs_pert,
+        n_obs_ctrl=n_obs_ctrl,
+        n_genes=n_genes,
+        obsm_keys_to_dim=obsm_keys_to_dim,
+        obs_columns_to_nunique_and_prefix=obs_columns_to_nunique_and_prefix,
+        uns_keys_to_nunique_prefix_and_dim=uns_keys_to_nunique_prefix_and_dim,
+        obs_columns_to_fixed_val=obs_columns_to_fixed_val,
+        control_key=control_key,
+    )

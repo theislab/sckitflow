9ff2079ff8f939d39766b9efec299d0c123db1c3
LeonStadelmann <leonstadlmnn@gmail.com>
2025-11-12 15:56:47 +0000

    Implement set encoder in Jax

--- src/sc_flow/backends/jax/nn/__init__.py
+++ src/sc_flow/backends/jax/nn/__init__.py
@@ -1,9 +1,13 @@
 from sc_flow.backends.jax.nn._modules import (
+    BaseModule,
+    FunctionalModule,
     MLP,
     Resnet1d,
 )
 
 __all__ = [
+    "BaseModule",
+    "FunctionalModule",
     "MLP",
     "Resnet1d",
 ]

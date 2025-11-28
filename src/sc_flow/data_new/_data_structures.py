ad6bd64fd85575f1fe59ee23200657dbbcdaf4ea
lorenzo-consoli <lorenzo.consoli@proton.me>
2025-11-28 18:36:12 +0100

    Continue to define data containers

--- src/sc_flow/data_new/_data_structures.py
+++ src/sc_flow/data_new/_data_structures.py
@@ -1,36 +1,75 @@
+from collections.abc import Collection
 from dataclasses import dataclass
 
 import numpy as np
 
-from sc_flow.data_new._mixins import ArrayMixin, BatchMixin
+from sc_flow._types import MappedArray
+from sc_flow.data_new._mixins import BatchMixin
 
-__all__ = ["DistributionData"]
+__all__ = [
+    "CategoricalDataContainer",
+    "StateDataContainer",
+    "TargetDataContainer",
+    "ConditionDataContainer",
+    "DistributionContainer",
+    "MatchedDistributionsContainer",
+]
 
 
 @dataclass
-class StateData:
+class BaseDataContainer:
+    """"""  # noqa
+
+    pass
+
+
+@dataclass
+class CategoricalDataContainer(BaseDataContainer):
+    """"""  # noqa
+
+    column_values: Collection[tuple[str]]
+    repr_dict: MappedArray
+
+
+@dataclass
+class StateDataContainer(BaseDataContainer):
+    """"""  # noqa
+
     X: np.ndarray
 
 
 @dataclass
-class TargetData:
+class TargetDataContainer(BaseDataContainer):
+    """"""  # noqa
+
     target_data: BatchMixin
 
 
 @dataclass
-class ConditionData:
-    condition_reps: dict[str, ArrayMixin]
+class ConditionDataContainer(BaseDataContainer):
+    """"""  # noqa
+
+    condition_reps: CategoricalDataContainer
     condition_covariates: BatchMixin
 
 
 @dataclass
-class DistributionData:
-    state_data: StateData
-    target_data: TargetData
-    condition_data: ConditionData
+class DistributionContainer(BaseDataContainer):
+    """"""  # noqa
+
+    state_data: StateDataContainer
+    target_data: TargetDataContainer
+    condition_data: ConditionDataContainer
 
 
 @dataclass
-class MatchedDistributionsData:
-    target_distr: DistributionData
-    source_distr: DistributionData | None = None
+class MatchedDistributionsContainer(BaseDataContainer):
+    """"""  # noqa
+
+    target_distr: DistributionContainer
+    source_distr: DistributionContainer | None = None
+
+    @property
+    def has_source(self) -> bool:
+        """"""  # noqa
+        return self.source_distr is not None

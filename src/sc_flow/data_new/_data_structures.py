0aaab83fe44092654bc88dbddd85d754a1463366
lorenzo-consoli <lorenzo.consoli@proton.me>
2025-11-28 17:26:18 +0100

    continuing draft core data containers.

--- src/sc_flow/data_new/_data_structures.py
+++ src/sc_flow/data_new/_data_structures.py
@@ -6,6 +6,11 @@ __all__ = ["DistributionData"]
 
 
 @dataclass
+class StateData:
+    X: np.ndarray
+
+
+@dataclass
 class TargetData: ...
 
 
@@ -15,6 +20,12 @@ class ConditionData: ...
 
 @dataclass
 class DistributionData:
-    X: np.ndarray
+    state_data: StateData
     target_data: TargetData
     condition_data: ConditionData
+
+
+@dataclass
+class MatchedDistributionsData:
+    target_distr: DistributionData
+    source_distr: DistributionData | None = None

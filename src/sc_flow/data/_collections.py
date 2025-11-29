6b3613e59fe91aac09a74708db70f289a166d85d
lorenzo-consoli <lorenzo.consoli@proton.me>
2025-11-29 14:16:01 +0100

    continuing draft of data structures

---
+++ src/sc_flow/data/_collections.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+import pandas as pd
+
+from sc_flow.data._indexer import HierarchicalIndexer
+from sc_flow.data._structures import CompiledData
+
+__all__ = [
+    "BaseCollection",
+    "TrainCollection",
+]
+
+
+@dataclass
+class BaseCollection:
+    """"""  # noqa
+
+    indexer: HierarchicalIndexer
+    data: CompiledData
+
+    def __post_init__(self) -> None:
+        """"""  # noqa
+        self._index = self._get_index()
+
+    def _get_index(self) -> pd.MultiIndex:
+        """"""  # noqa
+        return self.indexer.create_index(self.data.ann_df)
+
+    @property
+    def index(self) -> pd.MultiIndex:
+        """"""  # noqa
+        return self._index
+
+
+class TrainCollection(BaseCollection):
+    """"""  # noqa

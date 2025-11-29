32d3c84a1d942d5f8905286b448be0debcd083a4
lorenzo-consoli <lorenzo.consoli@proton.me>
2025-11-29 21:42:39 +0100

    Updating init files and imports

---
+++ src/sc_flow/data/grouping/__init__.py
@@ -0,0 +1,7 @@
+from sc_flow.data.grouping._indexer import HierarchicalIndexer
+from sc_flow.data.grouping._selector import IndexSelector
+
+__all__ = [
+    "HierarchicalIndexer",
+    "IndexSelector",
+]

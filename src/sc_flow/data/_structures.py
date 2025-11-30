cab1f2a131cf76c17a1e6ff4cac3377c435798cc
lorenzo-consoli <lorenzo.consoli@proton.me>
2025-11-30 19:02:41 +0100

    continuing to work on data module

--- src/sc_flow/data/_structures.py
+++ src/sc_flow/data/_structures.py
@@ -1,11 +1,13 @@
-from collections.abc import Mapping
+import abc
+from collections.abc import Hashable, Mapping
 from dataclasses import dataclass
 from dataclasses import field as dc_field
+from typing import TypeVar
 
 import numpy as np
 import pandas as pd
 
-from sc_flow._types import MappedArray, TargetCovariatesEncoderCls
+from sc_flow._types import MappedArray, NestedMappedLevelIndex, TargetCovariatesEncoderCls
 from sc_flow.data._mixins import BatchMixin
 
 __all__ = [
@@ -15,15 +17,23 @@ __all__ = [
     "TargetData",
     "ConditionData",
     "CompiledData",
-    "MatchedData",
 ]
 
 
+T = TypeVar("T", "NestedData", "NestedCompiledData", "NestedMatchedData")
+
+
 @dataclass(frozen=True)
-class BaseData:
+class BaseData(abc.ABC):
     """"""  # noqa
 
-    pass
+    @abc.abstractmethod
+    def slice_with_index(
+        self,
+        index: pd.MultiIndex,
+    ) -> "BaseData":
+        """"""  # noqa
+        raise NotImplementedError
 
 
 @dataclass(frozen=True)
@@ -33,6 +43,13 @@ class CategoricalData(BaseData):
     ann_df: pd.DataFrame
     repr_dict: dict[str, MappedArray] = dc_field(default_factory=lambda: {})
     categorical_encoders: Mapping[str, TargetCovariatesEncoderCls] = dc_field(default_factory=lambda: {})
+
+    def slice_with_index(
+        self,
+        index: pd.MultiIndex,
+    ) -> "CategoricalData":
+        """"""  # noqa
+        raise NotImplementedError
 
 
 @dataclass(frozen=True)
@@ -46,6 +63,13 @@ class StateData(BaseData):
 
     X: np.ndarray
 
+    def slice_with_index(
+        self,
+        index: pd.MultiIndex,
+    ) -> "StateData":
+        """"""  # noqa
+        raise NotImplementedError
+
 
 @dataclass(frozen=True)
 class TargetData(BaseData):
@@ -54,6 +78,13 @@ class TargetData(BaseData):
     categorical_covariates: CategoricalData | None = None
     continuous_covariates: BatchMixin | None = None
 
+    def slice_with_index(
+        self,
+        index: pd.MultiIndex,
+    ) -> "TargetData":
+        """"""  # noqa
+        raise NotImplementedError
+
 
 @dataclass(frozen=True)
 class ConditionData(BaseData):
@@ -61,6 +92,13 @@ class ConditionData(BaseData):
 
     condition_reps: CombinationData | None = None
     condition_covariates: BatchMixin | None = None
+
+    def slice_with_index(
+        self,
+        index: pd.MultiIndex,
+    ) -> "ConditionData":
+        """"""  # noqa
+        raise NotImplementedError
 
 
 @dataclass(frozen=True)
@@ -79,7 +117,55 @@ class CompiledData(BaseData):
         groups_df = self.groups_data.ann_df
         return pd.concat((conditions_df, groups_df), axis=1)
 
+    def slice_with_index(
+        self,
+        index: pd.MultiIndex,
+    ) -> "CompiledData":
+        """"""  # noqa
+        raise NotImplementedError
+
 
 @dataclass(frozen=True)
-class MatchedData:
+class NestedData(BaseData, abc.ABC):
+    """"""  # noqa
+
+    @classmethod
+    def init_from_nested_index(cls, data: BaseData, mapped_index: NestedMappedLevelIndex) -> "NestedData":
+        """"""  # noqa
+        raise NotImplementedError
+
+
+@dataclass(frozen=True)
+class NestedCompiledData(NestedData, CompiledData):
+    """"""  # noqa
+
+    data: Mapping[Hashable, CompiledData | T]
+    mapped_index: NestedMappedLevelIndex
+
+    @classmethod
+    def init_from_nested_index_dict(
+        cls, data: CompiledData, mapped_index: NestedMappedLevelIndex
+    ) -> "NestedCompiledData":
+        """"""  # noqa
+        return cls._init_from_nested_index_dict(
+            data,
+            mapped_index,
+        )
+
+    @classmethod
+    def _init_from_nested_index_dict(data: CompiledData, mapped_index: NestedMappedLevelIndex) -> "NestedCompiledData":
+        """"""  # noqa
+
+
+@dataclass(frozen=True)
+class MatchedData(BaseData):
+    """"""  # noqa
+
+    index: pd.MultiIndex
+    target_data: NestedCompiledData
+    source_data: CompiledData | None = None
+
+
+@dataclass(frozen=True)
+class NestedMatchedData(NestedData, MatchedData):
     """"""  # noqa

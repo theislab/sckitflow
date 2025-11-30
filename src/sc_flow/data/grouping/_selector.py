5f515365ce4d19ecb6b80b53a2a3172a83e8ba4c
lorenzo-consoli <lorenzo.consoli@proton.me>
2025-11-30 13:16:00 +0100

    finally cleaned up the code to handle grouping

--- src/sc_flow/data/grouping/_selector.py
+++ src/sc_flow/data/grouping/_selector.py
@@ -4,18 +4,22 @@ from typing import Any
 import numpy as np
 import pandas as pd
 
-from sc_flow._utils import check_sequence_query_against_reference
 from sc_flow.data.grouping._indexer import HierarchicalIndexer
+from sc_flow.data.grouping._query import QueryFactory
 
 __all__ = ["IndexSelector"]
 
 
-@dataclass(frozen=True)
+@dataclass
 class IndexSelector:
     """"""  # noqa
 
     registry: dict[str, tuple[str, ...] | None]
     hierarchy_levels: list[str]
+
+    def __post_init__(self) -> None:
+        """"""  # noqa
+        self._query_factory = QueryFactory(self.registry)
 
     @classmethod
     def init_from_indexer(cls, indexer: HierarchicalIndexer) -> "IndexSelector":
@@ -25,13 +29,6 @@ class IndexSelector:
             indexer._hierarchy_levels,
         )
 
-    @staticmethod
-    def _parse_query_dict(
-        query_dict: dict[str, Any],
-    ) -> tuple[Any]:
-        """"""  # noqa
-        return tuple(query_dict[cond] for cond in sorted(query_dict.keys()))
-
     def _get_level_query_mask(
         self,
         level_name: str,
@@ -39,45 +36,16 @@ class IndexSelector:
         index: pd.MultiIndex,
     ) -> pd.Series | np.ndarray:
         """"""  # noqa
-        # sanity checks
-        if level_name not in self.registry_keys:
-            msg = f"Level {level_name} not found in {self.registry_keys=}."
-            raise KeyError(msg)
-        if level_name not in index.names:
-            msg = f"Level {level_name} not found in {index.names=}."
-            raise KeyError(msg)
-        self._verify_level_query_dict(level_name, query_dict)
-
-        # retrieving level values and parsing query dict
+        self._query_factory.verify_level_query_dict(level_name, query_dict)
+        query_value = self._query_factory.query_dict_to_tuple(query_dict)
         level_values = index.get_level_values(level_name)
-        query_value = self._parse_query_dict(query_dict)
         return level_values == query_value
 
-    def _prepare_partial_level_query_dict(
-        self,
-        level_name: str,
-        query_dict: dict[str, Any],
-    ) -> dict[str, Any]:
+    def _get_unique_level_values(self, level_name: str, index: pd.MultiIndex) -> tuple[tuple[Any]]:
         """"""  # noqa
-        # retrieving all level columns
-        level_cols = sorted(self.registry[level_name])
-
-        # expanding query dict
-        expanded_query_dict = {}
-        for col in level_cols:
-            if col not in query_dict:
-                expanded_query_dict[col] = slice(None)
-            else:
-                expanded_query_dict[col] = query_dict[col]
-        return expanded_query_dict
-
-    def _prepare_partial_query_dict(
-        self,
-        query_dict: dict[str, dict[str, Any]],
-        index: pd.MultiIndex,
-    ) -> dict[str, Any]:
-        """"""  # noqa
-        raise NotImplementedError
+        self._query_factory.verify_valid_level_name(level_name, reference=index.names)
+        level_values = index.get_level_values(level_name).values
+        return map(tuple, set(level_values))
 
     def _query_level_with_dict(
         self,
@@ -89,18 +57,24 @@ class IndexSelector:
         mask = self._get_level_query_mask(level_name, query_dict, index)
         return index[mask]
 
+    def _query_level_with_tuple(
+        self,
+        level_name: str,
+        values: tuple[Any, ...],
+        index: pd.MultiIndex,
+    ) -> pd.MultiIndex:
+        """"""  # noqa
+        query_dict = self._query_factory.query_tuple_to_dict(self.registry, values, level_name)
+        return self._query_level_with_dict(level_name, query_dict, index)
+
     def _query_with_dict(
         self,
         query_dict: dict[str, dict[str, Any]],
         index: pd.MultiIndex,
     ) -> pd.MultiIndex:
         """"""  # noqa
-        # verifying dictionary
-        self._verify_query_dict(query_dict)
-
-        # iterating over each level in hierarchical order
+        self._query_factory.verify_query_dict(query_dict)
         for level_name in self.hierarchy_levels:
-            # querying current level
             level_query_dict = query_dict[level_name]
             index = self._query_level_with_dict(
                 level_name,
@@ -109,42 +83,35 @@ class IndexSelector:
             )
         return index
 
-    def _verify_level_query_dict(
+    def _unique_level_vals_to_dict(self, level_name: str, index: pd.MultiIndex) -> tuple[tuple[Any]]:
+        """"""  # noqa
+        unique_level_values = self._get_unique_level_values(level_name, index)
+        return {values: self._query_level_with_tuple(level_name, values, index) for values in unique_level_values}
+
+    def _level_index_to_nested_dict(
         self,
         level_name: str,
-        query_dict: dict[str, Any],
-    ) -> None:
+        index: pd.MultiIndex,
+    ) -> dict:
         """"""  # noqa
-        check_sequence_query_against_reference(
-            query_dict.keys(), self.registry[level_name], allow_missing_from_query=False
-        )
 
-    def _verify_query_dict(
-        self,
-        query_dict: dict[str, dict[str, Any]],
-    ) -> None:
-        """"""  # noqa
-        check_sequence_query_against_reference(query_dict.keys(), self.registry.keys())
-        for level_name, level_query_dict in query_dict.items():
-            self._verify_level_query_dict(level_name, level_query_dict)
-
-    def get_query_dict_from_tuple(
-        self,
-        values: tuple[Any],
-        level_name: str,
-    ) -> dict[str, Any]:
-        """"""  # noqa
-        if level_name not in self.registry_keys:
-            msg = f"Level {level_name} not found. Available options are {self.registry_keys}."
-            raise KeyError(msg)
-        level_columns = sorted(self.registry[level_name])
-        if len(values) != len(level_columns):
-            msg = (
-                f"The number of provided values is wrong, got {len(values)}"
-                f", expected {len(level_columns)} for level {level_name}"
-            )
-            raise ValueError(msg)
-        return {col: values[idx] for idx, col in enumerate(level_columns)}
+        # preparing level data
+        self._query_factory.verify_valid_level_name(level_name, reference=index.names)
+        hierarchy_index = self.hierarchy_levels.index(level_name)
+        current_level_unique_values_dict = self._unique_level_vals_to_dict(level_name, index)
+        # last level (create dict)
+        if hierarchy_index == (self.n_hierarchy_levels - 1):
+            return current_level_unique_values_dict
+        # intermediate level (recursive call on each leaf)
+        else:
+            next_level_name = self.hierarchy_levels[hierarchy_index + 1]
+            return {
+                values: self._level_index_to_nested_dict(
+                    next_level_name,
+                    values_index,
+                )
+                for values, values_index in current_level_unique_values_dict.items()
+            }
 
     def query_level_with_dict(
         self,
@@ -167,10 +134,22 @@ class IndexSelector:
     ) -> pd.MultiIndex:
         """"""  # noqa
         query_dict = self._prepare_partial_query_dict(query_dict)
-        return self._query_with_dict(
-            query_dict,
-            index,
-        )
+        return self._query_with_dict(query_dict, index)
+
+    def level_index_to_nested_dict(
+        self,
+        level_name: str,
+        index: pd.MultiIndex,
+    ) -> None:
+        """"""  # noqa
+        return self._level_index_to_nested_dict(level_name, index)
+
+    def index_to_nested_dict(
+        self,
+        index: pd.MultiIndex,
+    ) -> None:
+        """"""  # noqa
+        return self._level_index_to_nested_dict(self.hierarchy_levels[0], index)
 
     @property
     def n_hierarchy_levels(self) -> int:
@@ -181,3 +160,8 @@ class IndexSelector:
     def registry_keys(self) -> tuple[str]:
         """Returns the keys of the registry as a tuple."""
         return tuple(self.registry.keys())
+
+    @property
+    def query_factory(self) -> QueryFactory:
+        """"""  # noqa
+        return self._query_factory

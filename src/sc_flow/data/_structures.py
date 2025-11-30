611f24dc6c4c70ae5b8952720d8d3fbc578a2a97
lorenzo-consoli <lorenzo.consoli@proton.me>
2025-12-01 00:15:37 +0100

    proceeding further with data module

--- src/sc_flow/data/_structures.py
+++ src/sc_flow/data/_structures.py
@@ -2,7 +2,7 @@ import abc
 from collections.abc import Hashable, Mapping
 from dataclasses import dataclass
 from dataclasses import field as dc_field
-from typing import TypeVar
+from typing import Any, ClassVar, Generic, TypeVar
 
 import numpy as np
 import pandas as pd
@@ -12,7 +12,6 @@ from sc_flow.data._mixins import BatchMixin
 
 __all__ = [
     "CategoricalData",
-    "CombinationData",
     "StateData",
     "TargetData",
     "ConditionData",
@@ -20,7 +19,7 @@ __all__ = [
 ]
 
 
-T = TypeVar("T", "NestedData", "NestedCompiledData", "NestedMatchedData")
+T = TypeVar("T", bound="BaseData")
 
 
 @dataclass(frozen=True)
@@ -30,7 +29,8 @@ class BaseData(abc.ABC):
     @abc.abstractmethod
     def slice_with_index(
         self,
-        index: pd.MultiIndex,
+        reference_index: pd.MultiIndex,
+        query_index: pd.MultiIndex,
     ) -> "BaseData":
         """"""  # noqa
         raise NotImplementedError
@@ -46,15 +46,11 @@ class CategoricalData(BaseData):
 
     def slice_with_index(
         self,
-        index: pd.MultiIndex,
+        reference_index: pd.MultiIndex,
+        query_index: pd.MultiIndex,
     ) -> "CategoricalData":
         """"""  # noqa
         raise NotImplementedError
-
-
-@dataclass(frozen=True)
-class CombinationData(CategoricalData):
-    """"""  # noqa
 
 
 @dataclass(frozen=True)
@@ -65,7 +61,8 @@ class StateData(BaseData):
 
     def slice_with_index(
         self,
-        index: pd.MultiIndex,
+        reference_index: pd.MultiIndex,
+        query_index: pd.MultiIndex,
     ) -> "StateData":
         """"""  # noqa
         raise NotImplementedError
@@ -80,7 +77,8 @@ class TargetData(BaseData):
 
     def slice_with_index(
         self,
-        index: pd.MultiIndex,
+        reference_index: pd.MultiIndex,
+        query_index: pd.MultiIndex,
     ) -> "TargetData":
         """"""  # noqa
         raise NotImplementedError
@@ -90,12 +88,13 @@ class TargetData(BaseData):
 class ConditionData(BaseData):
     """"""  # noqa
 
-    condition_reps: CombinationData | None = None
+    condition_reps: CategoricalData | None = None
     condition_covariates: BatchMixin | None = None
 
     def slice_with_index(
         self,
-        index: pd.MultiIndex,
+        reference_index: pd.MultiIndex,
+        query_index: pd.MultiIndex,
     ) -> "ConditionData":
         """"""  # noqa
         raise NotImplementedError
@@ -119,53 +118,121 @@ class CompiledData(BaseData):
 
     def slice_with_index(
         self,
-        index: pd.MultiIndex,
+        reference_index: pd.MultiIndex,
+        query_index: pd.MultiIndex,
     ) -> "CompiledData":
         """"""  # noqa
-        raise NotImplementedError
-
-
-@dataclass(frozen=True)
-class NestedData(BaseData, abc.ABC):
-    """"""  # noqa
-
-    @classmethod
-    def init_from_nested_index(cls, data: BaseData, mapped_index: NestedMappedLevelIndex) -> "NestedData":
-        """"""  # noqa
-        raise NotImplementedError
-
-
-@dataclass(frozen=True)
-class NestedCompiledData(NestedData, CompiledData):
-    """"""  # noqa
-
-    data: Mapping[Hashable, CompiledData | T]
-    mapped_index: NestedMappedLevelIndex
-
-    @classmethod
-    def init_from_nested_index_dict(
-        cls, data: CompiledData, mapped_index: NestedMappedLevelIndex
-    ) -> "NestedCompiledData":
-        """"""  # noqa
-        return cls._init_from_nested_index_dict(
-            data,
-            mapped_index,
+        state_data = self.state_data.slice_with_index(reference_index, query_index)
+        target_data = self.target_data.slice_with_index(reference_index, query_index)
+        condition_data = self.condition_data.slice_with_index(reference_index, query_index)
+        groups_data = self.groups_data.slice_with_index(reference_index, query_index)
+        return self.__class__(
+            state_data, target_data=target_data, condition_data=condition_data, groups_data=groups_data
         )
 
+
+@dataclass(frozen=True)
+class NestedData(Generic[T]):
+    """"""  # noqa
+
+    dtype: ClassVar[type] = BaseData
+    data: Mapping[Hashable, "T | NestedData[T]"]
+
     @classmethod
-    def _init_from_nested_index_dict(data: CompiledData, mapped_index: NestedMappedLevelIndex) -> "NestedCompiledData":
+    def init_from_data(
+        cls,
+        data: BaseData,
+        reference_index: pd.MultiIndex,
+        mapped_index: NestedMappedLevelIndex,
+    ) -> "NestedData":
         """"""  # noqa
+        return cls._get_mapped_data_from_dict(data, reference_index, mapped_index)
+
+    @classmethod
+    def _get_leaf_mapped_data_from_dict(
+        cls,
+        data: BaseData,
+        reference_index: pd.MultiIndex,
+        mapped_index: NestedMappedLevelIndex,
+    ) -> "NestedData":
+        """"""  # noqa
+        if not isinstance(data, cls.dtype):
+            msg = f"Leaf data is expected to be of type {cls.dtype}, found {type(data)}."
+            raise TypeError(msg)
+        data_dict = {
+            values: data.slice_with_index(query_index, reference_index) for values, query_index in mapped_index.items()
+        }
+        return NestedData(data_dict)
+
+    @classmethod
+    def _get_mapped_data_from_dict(
+        cls,
+        data: BaseData,
+        reference_index: pd.MultiIndex,
+        mapped_index: NestedMappedLevelIndex,
+    ) -> "NestedData":
+        if isinstance(mapped_index, NestedData):
+            return cls._get_leaf_mapped_data_from_dict(
+                data,
+                reference_index,
+                mapped_index,
+            )
+        else:
+            return cls._get_leaf_mapped_data_from_dict(data, reference_index, mapped_index)
 
 
 @dataclass(frozen=True)
-class MatchedData(BaseData):
+class NestedCompiledData(NestedData):
     """"""  # noqa
 
-    index: pd.MultiIndex
+    dtype: ClassVar[type] = CompiledData
+
+
+@dataclass(frozen=True)
+class MatchedData:
+    """"""  # noqa
+
     target_data: NestedCompiledData
     source_data: CompiledData | None = None
 
+    @classmethod
+    def init_from_data(
+        cls,
+        data: CompiledData,
+        levels_source_value_dict: dict[str, Any],
+    ) -> "MatchedData":
+        """"""  # noqa
+        target_data: NestedCompiledData = cls._init_target_distributions(
+            data,
+            levels_source_value_dict,
+        )
+        source_data: CompiledData | None = cls._init_source_distribution(
+            data,
+            levels_source_value_dict,
+        )
+        return cls(target_data, source_data=source_data)
+
+    @classmethod
+    def _init_target_distributions(
+        cls,
+        data: CompiledData,
+        levels_source_value_dict: dict[str, Any],
+    ) -> NestedCompiledData:
+        """"""  # noqa
+        raise NotImplementedError
+
+    @classmethod
+    def _init_source_distribution(
+        cls,
+        data: CompiledData,
+        levels_source_value_dict: dict[str, Any],
+    ) -> CompiledData:
+        """"""  # noqa
+        raise NotImplementedError
+
 
 @dataclass(frozen=True)
-class NestedMatchedData(NestedData, MatchedData):
+class NestedMatchedData(NestedData):
     """"""  # noqa
+
+    dtype: ClassVar[type] = MatchedData

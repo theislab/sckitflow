55ab0c3e03f9c2fbc154be8cd33e1050ae3b8e08
lorenzo-consoli <lorenzo.consoli@proton.me>
2025-12-03 16:54:39 +0100

    added further tests to selector

--- tests/data/grouping/test_selector.py
+++ tests/data/grouping/test_selector.py
@@ -9,12 +9,12 @@ from sc_flow._constants import CONDITION_LEVEL_NAME, GROUP_LEVEL_NAME
 from sc_flow.data.grouping._indexer import HierarchicalIndexer
 from sc_flow.data.grouping._selector import IndexSelector
 
-wrong_key = "wrong_key"
+inval_key = "wrong_key"
 
 
 class TestIndexSelector:
     @pytest.mark.parametrize("groups_cols", [None, ["source_split"]])
-    @pytest.mark.parametrize("conditions_cols", [None, ["drugA", "drugB"]])
+    @pytest.mark.parametrize("conditions_cols", [None, ["drugA"], ["drugA", "drugB"]])
     @pytest.mark.parametrize("level_name", [CONDITION_LEVEL_NAME, GROUP_LEVEL_NAME])
     @pytest.mark.parametrize(
         "query_dict",
@@ -78,6 +78,14 @@ class TestIndexSelector:
                     index,
                 )
             return None
+        elif level_name == CONDITION_LEVEL_NAME and len(set(query_dict).difference(set(conditions_cols))) > 0:
+            with pytest.raises(ValueError):
+                query_res = selector.query_level_with_dict(
+                    level_name,
+                    query_dict,
+                    index,
+                )
+            return None
         elif level_name == GROUP_LEVEL_NAME and groups_cols is None:
             with pytest.raises(ValueError):
                 query_res = selector.query_level_with_dict(
@@ -91,4 +99,79 @@ class TestIndexSelector:
             query_dict,
             index,
         )
+        assert isinstance(query_res, pd.MultiIndex)
+
+    @pytest.mark.parametrize("groups_cols", [None, ["source_split"]])
+    @pytest.mark.parametrize("conditions_cols", [None, ["drugA", "drugB"]])
+    @pytest.mark.parametrize(
+        "query_dict",
+        [
+            {
+                CONDITION_LEVEL_NAME: {
+                    "drugA": ("control",),
+                    "drugB": ("control",),
+                },
+            },
+            {
+                CONDITION_LEVEL_NAME: {
+                    "drugA": ("control",),
+                },
+            },
+            {
+                GROUP_LEVEL_NAME: {
+                    "source_split": ("source_split0",),
+                },
+            },
+            {
+                CONDITION_LEVEL_NAME: {
+                    "drugA": ("control",),
+                    "drugB": ("control",),
+                },
+                GROUP_LEVEL_NAME: {
+                    "source_split": ("source_split0",),
+                },
+                inval_key: {
+                    "source_split": ("source_split0",),
+                },
+            },
+            {
+                CONDITION_LEVEL_NAME: {
+                    "source_split": ("source_split0",),
+                },
+            },
+        ],
+    )
+    def test_query_with_dict(
+        self,
+        adata: AnnData,
+        groups_cols: Collection[str] | None,
+        conditions_cols: Collection[str] | None,
+        query_dict: dict[str, Any],
+    ) -> None:
+        # initialize indexer and selector
+        indexer = HierarchicalIndexer(
+            groups_cols=groups_cols,
+            conditions_cols=conditions_cols,
+        )
+        selector = IndexSelector.init_from_indexer(indexer)
+
+        # creating index
+        index = indexer.create_index(adata.obs)
+        if inval_key in query_dict:
+            with pytest.raises(ValueError):
+                query_res = selector.query_with_dict(query_dict, index)
+            return None
+        elif CONDITION_LEVEL_NAME in query_dict and conditions_cols is None:
+            with pytest.raises(ValueError):
+                query_res = selector.query_with_dict(query_dict, index)
+            return None
+        elif CONDITION_LEVEL_NAME in query_dict and "source_split" in query_dict[CONDITION_LEVEL_NAME]:
+            with pytest.raises(ValueError):
+                query_res = selector.query_with_dict(query_dict, index)
+            return None
+        elif GROUP_LEVEL_NAME in query_dict and groups_cols is None:
+            with pytest.raises(ValueError):
+                query_res = selector.query_with_dict(query_dict, index)
+            return None
+        query_res = selector.query_with_dict(query_dict, index)
         assert isinstance(query_res, pd.MultiIndex)

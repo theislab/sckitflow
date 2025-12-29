f8ecdf9542d2466deea7655596bf871993152e78
lorenzo-consoli <lorenzo.consoli@proton.me>
2025-12-29 15:46:37 +0100

    cleaning up data module

--- src/sc_flow/data/_structures.py
+++ src/sc_flow/data/_structures.py
@@ -1,19 +1,20 @@
 import abc
-from collections.abc import Mapping
+from collections.abc import Collection, Mapping
 from dataclasses import dataclass
 from dataclasses import field as dc_field
+from types import MappingProxyType
+from typing import ClassVar, Literal, overload
 
 import numpy as np
 import pandas as pd
 
-from sc_flow._types import MappedArray, TargetCovariatesEncoderCls
-from sc_flow.data._mixins import BatchMixin
+from sc_flow._types import MappedArray, NestedMappedLevelIndex, TargetCovariatesEncoderCls
+from sc_flow.data._mixins import BatchMixin, DataMixin
 
 __all__ = [
     "BaseData",
     "CategoricalData",
     "StateData",
-    "MixedTypeData",
     "MixedTypeData",
     "CouplingData",
     "DistributionData",
@@ -37,7 +38,23 @@ class BaseData(abc.ABC):
         :param query_index: The query index.
         :type query_index: class: `pd.MultiIndex`
         """
+        if not reference_index.is_unique:
+            raise ValueError("Reference index must be unique.")
         return reference_index.get_indexer(query_index)
+
+    def _assert_same_n_obs(
+        self,
+        query_container: "BaseData",
+    ) -> None:
+        n_obs_ref = self.n_obs
+        n_obs_query = query_container.n_obs
+        if n_obs_ref != n_obs_query:
+            msg = (
+                "Query and reference should share the same number of observations, "
+                f"found {n_obs_ref} observations for reference and {n_obs_query} "
+                "observations for query."
+            )
+            raise ValueError(msg)
 
     @abc.abstractmethod
     def _slice_with_array(
@@ -53,6 +70,30 @@ class BaseData(abc.ABC):
         """
         raise NotImplementedError
 
+    @overload
+    def slice_with_index(
+        self, reference_index: pd.MultiIndex, query_index: pd.MultiIndex, return_index: Literal[False]
+    ) -> "BaseData": ...
+
+    @overload
+    def slice_with_index(
+        self, reference_index: pd.MultiIndex, query_index: pd.MultiIndex, return_index: Literal[True]
+    ) -> "tuple[BaseData, np.ndarray]": ...
+
+    def slice_with_index(
+        self, reference_index: pd.MultiIndex, query_index: pd.MultiIndex, return_index: bool = False
+    ) -> "BaseData | tuple[BaseData, np.ndarray]":
+        """"""  # noqa
+        idxs = self._get_query_idxs(reference_index, query_index)
+        if np.any(idxs < 0):
+            msg = "Query index contains entries not present in reference index."
+            raise KeyError(msg)
+
+        query_data = self._slice_with_array(idxs)
+        if return_index:
+            return query_data, idxs
+        return query_data
+
     def slice_with_array(
         self,
         idxs: np.ndarray,
@@ -62,27 +103,14 @@ class BaseData(abc.ABC):
         :param idxs: The array storing the indices used for slicing.
         :type idxs: class: `np.ndarray`
         """
+        idxs = np.asarray(idxs)
         return self._slice_with_array(idxs)
 
-    def slice_with_index(
-        self, reference_index: pd.MultiIndex, query_index: pd.MultiIndex, return_index_array: bool = False
-    ) -> "BaseData | tuple[BaseData, np.ndarray]":
-        """Slices the underlying data with a reference and a query index.
-
-        :param reference_index: The reference index.
-        :type reference_index: class: `pd.MultiIndex`
-
-        :param query_index: The query index.
-        :type query_index: class: `pd.MultiIndex`
-
-        :param return_index_array: Whether to return the array storing the indices. Defaults to `False`.
-        :type return_index_array: class: `bool`
-        """
-        idxs = self._get_query_idxs(reference_index, query_index)
-        query_data = self._slice_with_array(idxs)
-        if return_index_array:
-            return query_data, idxs
-        return query_data
+    @property
+    @abc.abstractmethod
+    def n_obs(self) -> int:
+        """"""  # noqa
+        raise NotImplementedError
 
 
 @dataclass(frozen=True)
@@ -106,12 +134,10 @@ class CategoricalData(BaseData):
     """
 
     ann_df: pd.DataFrame
-    repr_dict: dict[str, MappedArray] = dc_field(default_factory=lambda: {})
-    categorical_encoders: Mapping[str, TargetCovariatesEncoderCls] = dc_field(default_factory=lambda: {})
-
-    def __repr__(self) -> str:
-        cls = self.__class__.__name__
-        return f"{cls} with {len(self.ann_df)} observations.\n\t Columns: {self.ann_df.columns}"
+    repr_dict: dict[str, MappedArray] = dc_field(default_factory=lambda: MappingProxyType({}))
+    categorical_encoders: Mapping[str, TargetCovariatesEncoderCls] = dc_field(
+        default_factory=lambda: MappingProxyType({})
+    )
 
     def _slice_with_array(
         self,
@@ -119,6 +145,11 @@ class CategoricalData(BaseData):
     ) -> "CategoricalData":
         ann_df = self.ann_df.iloc[idxs]
         return self.__class__(ann_df, repr_dict=self.repr_dict, categorical_encoders=self.categorical_encoders)
+
+    @property
+    def n_obs(self) -> int:
+        """Returns the number of observations associated with the data object."""
+        return self.ann_df.shape[0]
 
 
 @dataclass(frozen=True)
@@ -135,15 +166,17 @@ class MixedTypeData(BaseData):
     categorical_covariates: CategoricalData | None = None
     continuous_covariates: BatchMixin | None = None
 
-    def __repr__(self) -> str:
-        cls = self.__class__.__name__
-        categorical_covariates = self.categorical_covariates.__repr__()
-        continuous_covariates_names = list(self.continuous_covariates.mapping.keys())
-        return (
-            f"{cls} with components:"
-            f"\n\t {categorical_covariates}"
-            f"\n\t Continuous covariates {continuous_covariates_names}"
-        )
+    # def __post_init__(self) -> None:
+    #     if self.categorical_covariates is not None and self.continuous_covariates is not None:
+    #         n_obs_cat = self.categorical_covariates.ann_df.shape[0]
+    #         n_obs_cont = next(iter(self.continuous_covariates.apply(lambda e: e.shape[0]).mapping.values()))
+    #         if n_obs_cat != n_obs_cont:
+    #             msg = (
+    #                 "Shape mismatch between categorical and continuous covariates. "
+    #                 f"Found {n_obs_cat} observations for categorical covariates and "
+    #                 f"{n_obs_cont} observations for the continuous covariates."
+    #             )
+    #             raise ValueError(msg)
 
     def _slice_with_array(
         self,
@@ -159,6 +192,20 @@ class MixedTypeData(BaseData):
             categorical_covariates=categorical_covariates, continuous_covariates=continuous_covariates
         )
 
+    @property
+    def n_obs(self) -> int:
+        """Returns the number of observations associated with the data object."""
+        if self.categorical_covariates is not None:
+            return self.categorical_covariates.n_obs
+
+        if self.continuous_covariates is not None:
+            dims = self.continuous_covariates.reference_dims
+            if len(dims) > 1:
+                msg = f"Continuous covariates should have only one reference dim, found {len(dims)}"
+                raise ValueError(msg)
+            return dims[0]
+        return 0
+
 
 @dataclass(frozen=True)
 class StateData(BaseData):
@@ -170,16 +217,17 @@ class StateData(BaseData):
 
     X: np.ndarray
 
-    def __repr__(self) -> str:
-        cls = self.__class__.__name__
-        return f"{cls} of shape {self.X.shape}."
-
     def _slice_with_array(
         self,
         idxs: np.ndarray,
     ) -> "StateData":
         X = self.X[idxs]
         return self.__class__(X)
+
+    @property
+    def n_obs(self) -> int:
+        """Returns the number of observations associated with the data object."""
+        return self.X.shape[0]
 
 
 @dataclass(frozen=True)
@@ -207,19 +255,23 @@ class CouplingData(BaseData):
     source_lin: StateData | None = None
     source_quad: StateData | None = None
 
-    def __repr__(self) -> str:
-        cls = self.__class__.__name__
-        n_obs, n_shared_dims = self.target_lin.shape
+    def __post_init__(self):
+        if self.target_quad is not None:
+            self.target_lin._assert_same_n_obs(self.target_quad)
 
-        return (
-            f"{cls} with {n_obs} observations."
-            f"\n\t # Shared dimensions: {n_shared_dims}"
-            f"\n\t # Target only dims: {self.target_quad.shape[1]}"
-            if self.target_quad is not None
-            else f"\n\t # Source only dims: {self.source_quad.shape[1]}"
-            if self.source_quad is not None
-            else ""
-        )
+        if self.source_lin is not None:
+            n_dims_source = self.source_lin.X.shape[1]
+            n_dims_target = self.target_lin.X.shape[1]
+            if n_dims_source != n_dims_target:
+                msg = (
+                    "The linear terms should share the same number of spatial dimensions for "
+                    f"source and target, found {n_dims_source} dimensions for source and "
+                    f"{n_dims_target} dimensions for target."
+                )
+                raise ValueError(msg)
+
+            if self.source_quad is not None:
+                self.source_lin._assert_same_n_obs(self.source_quad)
 
     def _slice_with_array(self, idxs) -> "CouplingData":
         target_lin = self.target_lin.slice_with_array(idxs)
@@ -232,6 +284,18 @@ class CouplingData(BaseData):
             source_lin=source_lin,
             source_quad=source_quad,
         )
+
+    @property
+    def n_obs_source(self) -> int:
+        """Returns the number of observations associated with the data object for source samples."""
+        if self.source_lin is not None:
+            return self.source_lin.n_obs
+        return 0
+
+    @property
+    def n_obs_target(self) -> int:
+        """Returns the number of observations associated with the data object for target samples."""
+        return self.target_lin.n_obs
 
 
 @dataclass(frozen=True)
@@ -259,16 +323,13 @@ class DistributionData(BaseData):
     condition_data: MixedTypeData | None = None
     groups_data: CategoricalData | None = None
 
-    def __repr__(self) -> str:
-        cls = self.__class__.__name__
-
-        return (
-            f"{cls} with components:"
-            f"\n\t {self.state_data.__repr__()}"
-            f"\n\t {self.target_data.__repr__()}"
-            f"\n\t {self.condition_data.__repr__()}"
-            f"\n\t {self.groups_data.__repr__()}"
-        )
+    def __post_init__(self) -> None:
+        if self.target_data is not None:
+            self.state_data._assert_same_n_obs(self.target_data)
+        if self.condition_data is not None:
+            self.state_data._assert_same_n_obs(self.condition_data)
+        if self.groups_data is not None:
+            self.state_data._assert_same_n_obs(self.groups_data)
 
     def _slice_with_array(
         self,
@@ -287,7 +348,106 @@ class DistributionData(BaseData):
 
     @property
     def ann_df(self) -> pd.DataFrame:
-        """The annotation data frame for the condition and grouping data."""
-        conditions_df = self.condition_data.categorical_covariates.ann_df
-        groups_df = self.groups_data.ann_df
-        return pd.concat((conditions_df, groups_df), axis=1)
+        """"""  # noqa
+        dfs = []
+        if self.condition_data and self.condition_data.categorical_covariates:
+            dfs.append(self.condition_data.categorical_covariates.ann_df)
+        if self.groups_data:
+            dfs.append(self.groups_data.ann_df)
+        return pd.concat(dfs, axis=1) if dfs else pd.DataFrame()
+
+    @property
+    def n_obs(self) -> int:
+        """Returns the number of observations associated with the data object."""
+        return self.state_data.n_obs
+
+
+@dataclass(frozen=True)
+class MatchedData(BaseData):
+    """Container class for matched data."""
+
+    target_data: DistributionData
+    source_data: DistributionData | None = None
+    coupling_data: CouplingData | None = None
+
+    def __post_init__(self):
+        if self.coupling_data is not None:
+            self.target_data._assert_same_n_obs(self.coupling_data.target_lin)
+            if self.coupling_data.target_quad is not None:
+                self.target_data._assert_same_n_obs(self.coupling_data.target_quad)
+
+            if self.source_data is not None:
+                if self.coupling_data.source_lin is None:
+                    msg = "When passing source data, coupling should contain source linear terms."
+                    raise ValueError(msg)
+                self.source_data._assert_same_n_obs(self.coupling_data.source_lin)
+                if self.coupling_data.source_quad is not None:
+                    self.source_data._assert_same_n_obs(self.coupling_data.source_quad)
+
+    @property
+    def n_obs(self) -> int:
+        """Returns the number of observations associated with the data object."""
+        return self.target_data.n_obs
+
+
+@dataclass(frozen=True)
+class NestedData(DataMixin):
+    """Recursively mapped container for matched data."""
+
+    strict: ClassVar[bool] = False
+    required_value_type: ClassVar[type] = DistributionData
+    # required_value_type: ClassVar[type] = MatchedData
+
+    @classmethod
+    def init_from_data(
+        cls,
+        data: DistributionData,
+        reference_index: pd.MultiIndex,
+        mapped_index: NestedMappedLevelIndex,
+        n_levels: int,
+        conditions: dict[str, Collection[str]] | None,
+        control_values_dict: dict[str, str] | None,
+    ) -> "NestedData":
+        """Initialized the recursive mapping from the input."""
+        return cls._get_mapped_data_from_dict(
+            data, reference_index, mapped_index, n_levels, conditions, control_values_dict
+        )
+
+    @classmethod
+    def _get_leaf_mapped_data_from_dict(
+        cls,
+        data: DistributionData,
+        reference_index: pd.MultiIndex,
+        mapped_index: NestedMappedLevelIndex,
+        conditions: dict[str, Collection[str]] | None,
+        control_values_dict: dict[str, str] | None,
+        # ) -> MatchedData:
+    ) -> DistributionData:
+        if not isinstance(data, cls.required_value_type):
+            msg = f"Leaf data is expected to be of type {cls.required_value_type}, found {type(data)}."
+            raise TypeError(msg)
+        data = data.slice_with_index(reference_index, mapped_index)
+        return data
+
+    @classmethod
+    def _get_mapped_data_from_dict(
+        cls,
+        # data: MatchedData,
+        data: DistributionData,
+        reference_index: pd.MultiIndex,
+        mapped_index: NestedMappedLevelIndex,
+        n_levels: int,
+        conditions: dict[str, Collection[str]] | None,
+        control_values_dict: dict[str, str] | None,
+    ) -> "NestedData":
+        out_dict = {}
+        for key, value in mapped_index.mapping.items():
+            if isinstance(value, NestedMappedLevelIndex):
+                out_dict[key] = cls._get_mapped_data_from_dict(
+                    data, reference_index, value, n_levels, conditions, control_values_dict
+                )
+            else:
+                out_dict[key] = cls._get_leaf_mapped_data_from_dict(
+                    data, reference_index, value, conditions, control_values_dict
+                )
+        return cls(out_dict)

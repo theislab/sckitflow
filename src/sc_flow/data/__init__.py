29fcf71cf914240901d7aebbfc38fe5fa2566d6e
lorenzo-consoli <lorenzo.consoli@proton.me>
2026-01-05 15:34:45 +0100

    starting to draft samplers

--- src/sc_flow/data/__init__.py
+++ src/sc_flow/data/__init__.py
@@ -1,6 +1,6 @@
 from sc_flow.data import _mixins as mixins
 from sc_flow.data import _utils as utils
-from sc_flow.data import containers, schemas, sim
+from sc_flow.data import containers, samplers, schemas, sim
 from sc_flow.data._composite import MatchedData, NestedData
 from sc_flow.data._manager import DataManager
 from sc_flow.data.grouping._indexer import HierarchicalIndexer
@@ -9,6 +9,7 @@ from sc_flow.data.grouping._selector import IndexSelector
 __all__ = [
     "containers",
     "mixins",
+    "samplers",
     "structures",
     "utils",
     "schemas",
---
+++ src/sc_flow/data/samplers/__init__.py
@@ -0,0 +1,5 @@
+from sc_flow.data.samplers._base import BatchDType, Sampler, TreeDType
+from sc_flow.data.samplers._train import TrainSampler
+from sc_flow.data.samplers._validation import ValidationSampler
+
+__all__ = ["TreeDType", "BatchDType", "Sampler", "TrainSampler", "ValidationSampler"]

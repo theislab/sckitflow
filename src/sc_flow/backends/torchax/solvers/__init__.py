b5168a615fe89fc56337c511fb6053a11f53cb84
lorenzo-consoli <b00794799@essec.edu>
2026-02-27 18:07:47 +0100

    rematched file names

--- src/sc_flow/backends/torchax/solvers/__init__.py
+++ src/sc_flow/backends/torchax/solvers/__init__.py
@@ -5,7 +5,11 @@ from sc_flow.backends.torchax.solvers._controllers import (
     PIDController,
     StepTo,
 )
+<<<<<<< HEAD:src/sc_flow/backends/torchax/solvers/__init__.py
 from sc_flow.backends.torchax.solvers._ode_solver import WrappedODESolver
+=======
+from sc_flow.backends.torch.solvers.wrapperjax._ode_solver import WrappedODESolver
+>>>>>>> 239cfe0 (rematched file names):src/sc_flow/backends/torch/solvers/wrapperjax/__init__.py
 
 __all__ = [
     "WrappedODESolver",

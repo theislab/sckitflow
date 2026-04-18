a7315f9e34c27f7dc327e891882d15993dd5b44c
lorenzo-consoli <b00794799@essec.edu>
2026-04-18 13:52:54 +0200

    updated docs for torch model

--- src/sc_flow/backends/torch/_model.py
+++ src/sc_flow/backends/torch/_model.py
@@ -22,6 +22,25 @@ __all__ = ["SCFlow"]
 
 
 class SCFlow:
+    """Class for handling flow-based generative models with the torch backend.
+
+    :param _dm_cls: The data manager associate to the current class.
+        Defaults to `None`. To set it, you need to call the `register_adata`
+        class method before instatiating a `SCFlow` object.
+    :type DataManager: class: `DataManager | None`
+
+    :param _dims_registry: The data dimensionality registry associate to the current class.
+        Defaults to `None`. To set it, you need to call the `register_adata`
+        class method before instatiating a `SCFlow` object.
+    :type _dims_registry: class: `DataDimensionalitiesRegistry | None`
+
+    :param _is_paired_setting_cls: Whether the data is condigured in a paired mode.
+        Defaults to `False`. It will be automatically inferred
+        from the schema during the call to the `register_adata`
+        class method, before instatiating a `SCFlow` object.
+    :type _is_paired_setting_cls: class: `DataManager | None`
+    """
+
     _dm_cls: DataManager | None = None
     _dims_registry: DataDimensionalitiesRegistry | None = None
     _is_paired_setting_cls: bool = False
@@ -32,7 +51,14 @@ class SCFlow:
         adata: AnnData,
         **kwargs,
     ) -> None:
-        """"""  # noqa
+        """Registers the input adata as a class attribute using the provided schema settings.
+
+        :param adata: The input adata to register.
+        :type adata: class: `AnnData`
+
+        :param **kwargs: Other key-word arguments used to initialize the `DataManager`.
+        :type **kwargs: class: `dict[str, Any]`
+        """
         # initialize data manager
         cls._dm_cls = DataManager(**kwargs)
         cls._dims_registry = cls._dm_cls.get_data_dimensionalities(adata)
@@ -61,8 +87,84 @@ class SCFlow:
         plan_kwargs: dict[str, Any] | None = None,
         **kwargs,
     ) -> None:
-        """"""  # noqa
+        """Initializes the model.
 
+        :param *args: Positional arguments used to initialize the method class.
+        :type *args: class: `Sequence[Any]`
+
+        :param method_cls: Reference to a `BaseGenerativeFlow` class, used as the
+            underlying method. When provided, it will take priority over the method
+            set with :param: `method_id`. Defaults to `None`, in which case the
+            :param: `method_id` will be required.
+        :type method_cls: class: `type[BaseGenerativeFlow] | None`
+
+        :param method_id: String identifier for the current method to instantiate.
+            Defaults to `None`.
+        :type method_id: class: `AVAILABLE_METHODS`
+
+        :param probability_path: Instance of the conditional probability path
+            used to instantiate the flow model. Defaults to `None`.
+        :type probability_path: class: `BaseProbabilityPath | None`
+
+        :param match_fn: Function used to match the source and target groups.
+            Defaults to `None`.
+        :type match_fn: class: `TMatchFn | None`
+
+        :param noise_sampler: Function used to sample random noise during training.
+            Defaults to `None`.
+        :type noise_sampler: class: `TNoiseSamplerFn | None`
+
+        :param time_sampler: Function used to sample random time indices during training.
+            Defaults to `None`.
+        :type time_sampler: class: `TNoiseSamplerFn | None`
+
+        :param generate_from_noise: Flag indicating whether the define the interpolation
+            from noise rather than from a source distribution. Only used for the paired setting.
+            That is, when the `register_adata` class method infers the :attr: `is_paired_setting` as `True`.
+            Otherwise, this attribute will be automatically set to `True`, due to the absence of source states.
+            Defaults to `False`.
+        :type generate_from_noise: class: `bool`
+
+        :param dtype: Data type for the computations. Defaults to `torch.float32`.
+        :type dtype: class: `torch.dtype`
+
+        :param device_id: Identifier for the device used for the computations.
+            When available, defaults to "cuda". Otherwise is set to "cpu".
+        :type device_id: class: `str`
+
+        :param solver_cls: Reference to a `BaseSolver` class, used during inference. Defaults to `None`.
+        :type solver_cls: class: `type[BaseSolver] | None`
+
+        :param solver_kwargs: Keyword arguments used to initialize the solver class. Defaults to `None`
+        :type solver_kwargs: class: `dict[str, Any] | None`
+
+        :param optimizer_cls: Reference to a `torch.optim.Optimizer` class, used to update the model's weights. Defaults to `torch.optim.Adam`.
+        :type optimizer_cls: class: `type[torch.optim.Optimizer] | None`
+
+        :param optimizer_kwargs: Keyword arguments used to initialize the optimizer class, defaults to `None`.
+        :type optimizer_kwargs: class: `dict[str, Any] | None`
+
+        :param lr: The learning rate used for optimization. Defaults to `5e-5`.
+        :type lr: class: `float`
+
+        :param lr_scheduler_cls: Reference to a `torch.optim.lr_scheduler.LRScheduler` class,
+            used to adapt the learning rate during optimization. Defaults to `None`.
+        :type lr_scheduler_cls: class: `type[torch.optim.lr_scheduler.LRScheduler] | None`
+
+        :param lr_scheduler_kwargs: Keyword arguments used to initialize the learning rate scheduler class, defaults to `None`.
+            Only used when :param: `lr_scheduler_cls` is not `None`.
+        :type lr_scheduler_kwargs: class: `dict[str, Any] | None`
+
+        :param lr_scheduler_step: Flag indicating whether the lr scheduler step should be performed after a training iteration
+            or after a validation pass. Defaults to `"train_step"`. Only used when :param: `lr_scheduler_cls` is not `None`.
+        :type lr_scheduler_step: class: `Literal["train_step", "validation_step"]`
+
+        :param plan_kwargs: Additional arguments for the optimization plan.
+        :type plan_kwargs: class: `dict[str, Any] | None`
+
+        :param *kwargs: Keyword arguments used to initialize the method class.
+        :type *kwargs: class: `dict[str, Any]`
+        """
         # check that data was prepared
         if self.__class__._dm_cls is None:
             raise RuntimeError(
@@ -118,9 +220,10 @@ class SCFlow:
     def train(
         self,
         train_adata: AnnData,
-        n_train_steps: int = 10_000,
-        valid_freq: int = 1_000,
+        *args,
         val_adatas_dict: dict[str, AnnData] | None = None,
+        n_train_steps: int = 100_000,
+        valid_freq: int = 1_000,
         train_batch_size: int = 128,
         train_n_nodes: int = 1,
         train_replace_samples: bool = False,
@@ -131,8 +234,74 @@ class SCFlow:
         val_replace_samples: bool = False,
         val_replace_nodes: bool = False,
         val_use_nodes_weights: bool = True,
+        **kwargs,
     ) -> None:
-        """"""  # noqa
+        """Trains the model on the input adata.
+
+        :param train_adata: The train adata.
+        :type train_adata: class: `AnnData`
+
+        :param *args: Positional arguments used to initialize the trainer class.
+        :type *args: class: `Sequence[Any]`
+
+        :param val_adatas_dict: Dictionary containing the validation adatas.
+        :type val_adatas_dict: class: `dict[str, AnnData]`
+
+        :param n_train_steps: The number of training steps to train the model over.
+            Defaults to `10_000`
+        :type n_train_steps: class: `int`
+
+        :param valid_freq: The frequency of the validation steps during training.
+            Defaults to `1_000`
+        :type valid_freq: class: `int`
+
+        :param train_batch_size: The number of observations to sample for each node in a batch
+            of training data. Defaults to `128`.
+        :type train_batch_size: class: `int`
+
+        :param train_n_nodes: The number of nodes to sample for each batch of training data.
+            Defaults to :constant sc_flow._constants.DEFAULT_N_GROUPS:.
+        :type train_n_nodes: class: `int`
+
+        :param train_replace_samples: Whether to sample training observations with replacement
+            from each node. Defaults to `False`.
+        :type train_replace_samples: class: `bool`
+
+        :param train_replace_nodes: Whether to sample trainining nodes with replacement
+            from the tree. Defaults to `False`.
+        :type train_replace_nodes: class: `bool`
+
+        :param train_use_nodes_weights: Whether to use nodes weights in order to sample training data.
+            Each node weight is computed as its relative frequency over the whole tree.
+            In order to compute the relative frequency of a node of matched distributions,
+            only the target one is considered. Defaults to `True`.
+        :type train_use_nodes_weights: class: `bool`
+
+        :param val_max_n_obs: The maximum number of observations to sample for each node in a batch
+            of validation data. Defaults to `10_000`.
+        :type val_max_n_obs: class: `int`
+
+        :param val_n_nodes: The number of nodes to sample for each batch of validation data.
+            Defaults to :constant sc_flow._constants.DEFAULT_N_GROUPS:.
+        :type val_n_nodes: class: `int`
+
+        :param val_replace_samples: Whether to sample validation observations with replacement
+            from each node. Defaults to `False`.
+        :type val_replace_samples: class: `bool`
+
+        :param val_replace_nodes: Whether to sample validation nodes with replacement
+            from the tree. Defaults to `False`.
+        :type val_replace_nodes: class: `bool`
+
+        :param val_use_nodes_weights: Whether to use nodes weights in order to sample validation data.
+            Each node weight is computed as its relative frequency over the whole tree.
+            In order to compute the relative frequency of a node of matched distributions,
+            only the target one is considered. Defaults to `True`.
+        :type val_use_nodes_weights: class: `bool`
+
+        :param *kwargs: Keyword arguments used to initialize the trainer class.
+        :type *kwargs: class: `dict[str, Any]`
+        """
         # compile adata
         train_tree = self._dm.compile_adata(train_adata)
         if val_adatas_dict is not None:
@@ -164,7 +333,7 @@ class SCFlow:
         }
 
         # initialize trainer
-        self._trainer = Trainer(self._method)
+        self._trainer = Trainer(self._method, *args, **kwargs)
 
         # module in training mode
         self._method.module.train()
@@ -183,17 +352,24 @@ class SCFlow:
         """
         Generates flow predictions.
 
+        :param adata: The input adata containing the metadata for prediction.
+        :type adata: class: `AnnData`
+
         :param return_tensors: If True, returns the raw concatenated PredictionData
-                            keeping the computation graph alive.
+            keeping the computation graph alive. Defaults to `False`.
+        :type return_tensors: class: `bool`
         """
+        # prepare module and data tree
         self._method.module.eval()
         tree = self._dm.compile_adata(adata)
         tree_flat: tuple[MatchedData] = tree.flatten()
 
+        # store for samples and metadata
         all_samples = []
         all_trajs = []
         all_obs = []
 
+        # iterate over each node
         pbar = tqdm(tree_flat, desc="Predicting")
         for node in pbar:
             # 1. Inference (Differentiable)
@@ -239,20 +415,20 @@ class SCFlow:
 
     @property
     def dm(self) -> DataManager:
-        """"""  # noqa
+        """Returns the data manager associated to the current instance."""
         return self._dm
 
     @property
-    def is_paired_setting(self) -> DataManager:
-        """"""  # noqa
+    def is_paired_setting(self) -> bool:
+        """Whether the data was registered in a paired setting."""
         return self._is_paired_setting
 
     @property
-    def method(self) -> DataManager:
-        """"""  # noqa
+    def method(self) -> BaseGenerativeFlow:
+        """Returns the underlying method."""
         return self._method
 
     @property
     def trainer(self) -> Trainer:
-        """"""  # noqa
+        """Returns the trainer used to fit the model."""
         return self._trainer

b0a244c7efcc00a6abbcd4152a6521bb4efa19ef
lorenzo-consoli <b00794799@essec.edu>
2026-04-19 02:15:57 +0200

    updating model calss

--- src/sc_flow/backends/jax/methods/__init__.py
+++ src/sc_flow/backends/jax/methods/__init__.py
@@ -1,1 +1,5 @@
-from sc_flow.backends.jax.methods._methods import GENOT, FlowMatching, OTFlowMatching
+from sc_flow.backends.jax.methods._methods import GENOT, FlowMatching
+
+METHODS_REGISTRY = {}
+
+__all__ = ["GENOT", "FlowMatching", "METHODS_REGISTRY"]

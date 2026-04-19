e997a7372187b07b09789619933a8e24a711e31a
lorenzo-consoli <b00794799@essec.edu>
2026-04-19 14:15:07 +0200

    added possibility to create custom methods

--- src/sc_flow/methods/__init__.py
+++ src/sc_flow/methods/__init__.py
@@ -1,3 +1,4 @@
+from sc_flow.methods._custom import register_method
 from sc_flow.methods._methods import BaseGenerativeFlow, BaseMethod
 
-__all__ = ["BaseMethod", "BaseGenerativeFlow"]
+__all__ = ["BaseMethod", "BaseGenerativeFlow", "register_method"]

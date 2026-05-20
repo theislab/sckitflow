5a47b4f4be4c2b2e951e05e784f9f526482eb366
lorenzo-consoli <b00794799@essec.edu>
2026-05-20 19:28:46 +0200

    initializing trainer only when it is none

--- src/sc_flow/_model.py
+++ src/sc_flow/_model.py
@@ -368,8 +368,9 @@ class SCFlow:
         else:
             raise_runtime_error_on_backend_not_supported(self._backend)
 
-        # initialize trainer (now passing optimization manager)
-        self._trainer = Trainer(self._method, opt_manager, callbacks)
+        # initialize trainer
+        if self._trainer is None:
+            self._trainer = Trainer(self._method, opt_manager, callbacks)
 
         # module in training mode
         self._method.set_train_mode(True)

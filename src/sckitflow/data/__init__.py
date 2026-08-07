475e8a7fe996b3e502fa414e4875316a1c9c0f35
selmanozleyen <syozleyen@gmail.com>
2026-08-07 15:06:17 +0200

    remove tree grouping and samplers

--- src/sckitflow/data/__init__.py
+++ src/sckitflow/data/__init__.py
@@ -2,11 +2,8 @@ from sckitflow.data import _dims_registry as dims_registry
 from sckitflow.data import _group_encoders as group_encoders
 from sckitflow.data import _mixins as mixins
 from sckitflow.data import _utils as utils
-from sckitflow.data import containers, samplers, schemas, sim, splitters
-from sckitflow.data._composite import MatchedData, NestedData
+from sckitflow.data import containers, schemas, sim, splitters
 from sckitflow.data._manager import DataManager
-from sckitflow.data.grouping._indexer import HierarchicalIndexer
-from sckitflow.data.grouping._selector import IndexSelector
 from sckitflow.data.splitters import CombinationSplitter, Splitter
 
 __all__ = [
@@ -14,16 +11,11 @@ __all__ = [
     "dims_registry",
     "group_encoders",
     "mixins",
-    "samplers",
     "utils",
     "schemas",
     "sim",
     "splitters",
     "DataManager",
-    "HierarchicalIndexer",
-    "IndexSelector",
-    "MatchedData",
-    "NestedData",
     "Splitter",
     "CombinationSplitter",
 ]
